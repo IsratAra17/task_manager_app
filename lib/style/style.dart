399f43ffe58bb7827403b36ceece69db15a7d0d4
s.m.israt <smisratara25@gmail.com>
2023-05-16 23:28:26 +0600

    Error Toast created

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -110,3 +110,16 @@ void SuccessToast(msg){
   );
 
 }
+void ErrorToast(msg)
+{
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: colorRed,
+    textColor: colorWhite,
+    fontSize: 16.0,
+  );
+
+}
