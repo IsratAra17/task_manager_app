0efe792d29bc56e796bd9a544d7d8519ac62089c
s.m.israt <smisratara25@gmail.com>
2023-05-16 23:12:08 +0600

    dropDown box decoration style done

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -1,51 +1,58 @@
 import 'package:flutter/material.dart';
 
-
-
 const colorRed = Color.fromRGBO(231, 28, 36, 1);
 const colorDark = Color.fromRGBO(136, 28, 32, 1);
-const colorGreen=Color.fromRGBO(33, 191, 115, 1);
-const colorBlue=Color.fromRGBO(52, 152, 219,1.0);
-const colorOrange=Color.fromRGBO(230, 126, 34,1.0);
-const colorWhite=Color.fromRGBO(255, 255, 255,1.0);
-const colorDarkBlue=Color.fromRGBO(44, 62, 80,1.0);
-const colorLightGray=Color.fromRGBO(135, 142, 150, 1.0);
-const colorLight=Color.fromRGBO(211, 211, 211, 1.0);
+const colorGreen = Color.fromRGBO(33, 191, 115, 1);
+const colorBlue = Color.fromRGBO(52, 152, 219, 1.0);
+const colorOrange = Color.fromRGBO(230, 126, 34, 1.0);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1.0);
+const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1.0);
+const colorLightGray = Color.fromRGBO(135, 142, 150, 1.0);
+const colorLight = Color.fromRGBO(211, 211, 211, 1.0);
 
-
-TextStyle Head1Text(textColor)
-{
+TextStyle Head1Text(textColor) {
   return TextStyle(
     color: textColor,
     fontSize: 28,
     fontFamily: 'poppins',
     fontWeight: FontWeight.w700,
-
   );
-
 }
-TextStyle Head6Style(textColor)
-{
+
+TextStyle Head6Style(textColor) {
   return TextStyle(
-    fontSize:16,
-    fontFamily:'poppins' ,
+    fontSize: 16,
+    fontFamily: 'poppins',
     fontWeight: FontWeight.w400,
   );
 }
 
-InputDecoration AppInputDecoration(label)
-{
+InputDecoration AppInputDecoration(label) {
   return InputDecoration(
-focusedBorder: const OutlineInputBorder(
-  borderSide: const BorderSide(color: colorGreen,width: 1),
-),
+    focusedBorder: const OutlineInputBorder(
+      borderSide: const BorderSide(color: colorGreen, width: 1),
+    ),
     fillColor: colorWhite,
     filled: true,
     contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
     enabledBorder: const OutlineInputBorder(
-      borderSide: const BorderSide(color: colorWhite,width: 0.0),
+      borderSide: const BorderSide(color: colorWhite, width: 0.0),
     ),
     border: OutlineInputBorder(),
     labelText: label,
+  );
+}
+
+DecoratedBox AppDropDownStyle(child) {
+  return DecoratedBox(
+    decoration: BoxDecoration(
+      color: colorWhite,
+      border: Border.all(color: Colors.white, width: 1),
+      borderRadius: BorderRadius.circular(4),
+    ),
+    child: Padding(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      child: child,
+    ),
   );
 }
