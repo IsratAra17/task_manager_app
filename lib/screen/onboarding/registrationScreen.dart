1af94d723d5712c1f7c076390cc9a9a7062ba7eb
s.m.israt <smisratara25@gmail.com>
2023-05-17 15:01:23 +0600

    Registration Page UI completed

--- lib/screen/onboarding/registrationScreen.dart
+++ lib/screen/onboarding/registrationScreen.dart
@@ -16,7 +16,41 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       body: Stack(
         children: [
           ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Join With Us",style: Head1Text(colorDarkBlue),),
+                  SizedBox(height: 1,),
+                  Text("Learn with israt ara",style: Head6Style(colorLightGray),),
+SizedBox(height: 20,),
+                  TextFormField(decoration: AppInputDecoration('First Name'),),
+                  SizedBox(height: 20,),
 
+                  TextFormField(decoration: AppInputDecoration('Last Name'),),
+                  SizedBox(height: 20,),
+
+                  TextFormField(decoration: AppInputDecoration('Mobile'),),
+                  SizedBox(height: 20,),
+
+                  TextFormField(decoration: AppInputDecoration('Password'),),
+                  SizedBox(height: 20,),
+
+                  TextFormField(decoration: AppInputDecoration('Confirm Password'),),
+                  SizedBox(height: 20,),
+
+                  Container(child: ElevatedButton(
+                    style: AppButtonStyle(),
+                    onPressed:(){},child: SuccessButtonChild('Registration'),),),
+
+
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
