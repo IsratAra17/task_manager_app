b6eef43daa71e57be282ea0c8685ff59004fc0a3
s.m.israt <smisratara25@gmail.com>
2023-05-17 15:26:23 +0600

    Email Verification UI done

--- lib/screen/onboarding/emailVerificationScreen.dart
+++ lib/screen/onboarding/emailVerificationScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taskmanager/style/style.dart';
 
 class EmailVerification extends StatefulWidget {
   const EmailVerification({Key? key}) : super(key: key);
@@ -10,6 +12,32 @@ class EmailVerification extends StatefulWidget {
 class _EmailVerificationState extends State<EmailVerification> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            height: double.infinity,
+            padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Text(('Your Email Address'),style: Head1Text(colorDarkBlue),),
+              SizedBox(height: 1,),
+              Text('A 6 digit pin will send to your email address',style: Head6Style(colorLightGray),),
+              SizedBox(height: 20,),
+              TextFormField(decoration: AppInputDecoration('Email Address'),),
+              Container(child: ElevatedButton(style:AppButtonStyle(),onPressed:(){},child: SuccessButtonChild('Next'),),),
+
+            ],
+          ),
+
+          ),
+
+
+
+
+        ],
+      ),
+    );
   }
 }
