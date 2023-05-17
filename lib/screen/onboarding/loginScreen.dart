7995ae9f966a43e011570a1bf77bef8eacff0099
s.m.israt <smisratara25@gmail.com>
2023-05-17 14:43:02 +0600

    Login page UI done

--- lib/screen/onboarding/loginScreen.dart
+++ lib/screen/onboarding/loginScreen.dart
@@ -13,43 +13,47 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ScreenBackground(context),
-        Padding(padding: EdgeInsets.all(30)),
-        Container(
-            child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Get Started With', style: Head1Text(colorDarkBlue)),
-              SizedBox(
-                height: 1,
-              ),
-              Text('Learn with Ara', style: Head6Style(colorLightGray)),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: AppInputDecoration("Email Address"),
-              ),
-              TextFormField(
-                decoration: AppInputDecoration("Password"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: ElevatedButton(
-                    style: AppButtonStyle(),
-                    onPressed: () {},
-                    child: SuccessButtonChild('Login')),
-              ),
-            ],
-          ),
-        )),
-      ],
+    return Scaffold(
+      body:
+       Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+           padding: EdgeInsets.all(30),
+
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Get Started With', style: Head1Text(colorDarkBlue)),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Text('Learn with Ara', style: Head6Style(colorLightGray)),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: AppInputDecoration("Email Address"),
+                    ),
+                    TextFormField(
+                      decoration: AppInputDecoration("Password"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: ElevatedButton(
+                          style: AppButtonStyle(),
+                          onPressed: () {},
+                          child: SuccessButtonChild('Login')),
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
