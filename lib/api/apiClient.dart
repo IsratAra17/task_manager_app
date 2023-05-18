405942e0cfdd58f6392a3114dbccd8825c438c98
s.m.israt <smisratara25@gmail.com>
2023-05-18 23:15:43 +0600

    LoginResponse basic code done

--- lib/api/apiClient.dart
+++ lib/api/apiClient.dart
@@ -4,3 +4,29 @@ import 'package:http/http.dart' as http;
 
 var BaseURL = "https://task.teamrabbil.com/api/v1";
 var RequestHeader = {"Content-Type": "application/json"};
+
+
+Future<bool>LoginRequest(FormValues) async {
+  var URL = Uri.parse("${BaseURL}/login");
+  var PostBody = json.encode(FormValues);
+  var response = await http.post(URL, headers: RequestHeader, body: PostBody);
+
+
+  var ResultCode = response.statusCode;
+  var ResultBody = json.decode(response.body);
+
+
+  if(ResultCode==200 && ResultBody['status']=='success')
+  {
+      SuccessToast("Request Success!");
+
+      return true;
+  }
+  else
+  {
+      ErrorToast("Request Failed! Try again!!");
+      return false;
+  }
+}
+
+
