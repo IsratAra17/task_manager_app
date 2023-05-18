91ac10a308266614dacd5e42a06c561dacca0797
s.m.israt <smisratara25@gmail.com>
2023-05-18 22:48:49 +0600

    declare globally to variable BaseUrl and Request header

--- lib/api/apiClient.dart
+++ lib/api/apiClient.dart
@@ -1,3 +1,6 @@
 import 'dart:convert';
 import 'package:taskmanager/style/style.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
+
+var BaseURL = "https://task.teamrabbil.com/api/v1";
+var RequestHeader = {"Content-Type": "application/json"};
