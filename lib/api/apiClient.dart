8a543ddfb81cae9e6858b18b3f286f3ca38c99fb
s.m.israt <smisratara25@gmail.com>
2023-05-18 23:27:11 +0600

    Set Password request added

--- lib/api/apiClient.dart
+++ lib/api/apiClient.dart
@@ -76,3 +76,48 @@ Future<bool>VerifyEmailRequest(Email)async{
     }
 }
 
+Future<bool>VerifyOTPrequest(Email,OTP)async{
+
+    var URL=Uri.parse("${BaseURL}/RecoverVerifyOTP/${Email}/${OTP}");
+
+    var response = await http.get(URL, headers: RequestHeader);
+
+    var ResultCode =response.statusCode;
+    var ResultBody = json.decode(response.body);
+
+
+    if(ResultCode==200 && ResultBody['status']=='success')
+    {
+        SuccessToast("Request Success!");
+
+        return true;
+    }
+    else
+    {
+        ErrorToast("Request Failed! Try again!!");
+        return false;
+    }
+}
+Future<bool>SetPasswordRequest(FormValues) async {
+    var URL=Uri.parse("${BaseURL}/RecoverResetPass");
+g    var PostBody = json.encode(FormValues);
+    var response = await http.post(URL, headers: RequestHeader, body: PostBody);
+
+
+    var ResultCode = response.statusCode;
+    var ResultBody = json.decode(response.body);
+
+
+    if(ResultCode==200 && ResultBody['status']=='success')
+    {
+        SuccessToast("Request Success!");
+
+        return true;
+    }
+    else
+    {
+        ErrorToast("Request Failed! Try again!!");
+        return false;
+    }
+}
+
