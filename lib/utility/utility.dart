e6386162979774e114566603bd190fb23ddd43ae
s.m.israt <smisratara25@gmail.com>
2023-05-23 20:08:01 +0600

    Create instance

--- lib/utility/utility.dart
+++ lib/utility/utility.dart
@@ -1,5 +1,9 @@
 
-void StoreUserData(userData){
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> StoreUserData(userData) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
 
 
 }
