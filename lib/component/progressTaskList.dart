1d9d5670ab7237f9993c8c7c45a9698992d9795b
s.m.israt <smisratara25@gmail.com>
2023-05-27 23:41:45 +0600

    progress task api call done

--- lib/component/progressTaskList.dart
+++ lib/component/progressTaskList.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import '../api/apiClient.dart';
+
 class progressTaskList extends StatefulWidget {
   const progressTaskList({Key? key}) : super(key: key);
 
@@ -8,6 +10,25 @@ class progressTaskList extends StatefulWidget {
 }
 
 class _progressTaskListState extends State<progressTaskList> {
+  List TaskItems=[];
+  bool Loading=true;
+  @override
+  void initState()
+  {
+    CallData();
+    super.initState();
+  }
+
+  CallData()async{
+    var data=await TaskListRequest("Progress");
+    setState(() {
+      Loading=false;
+      TaskItems=data;
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Center(child:Text("Progress task"));
