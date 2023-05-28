bf96235d1c9f2510e27d470e76e91252231640b1
s.m.israt <smisratara25@gmail.com>
2023-05-28 10:10:13 +0600

    Icon button added

--- lib/component/TaskAppBar.dart
+++ lib/component/TaskAppBar.dart
@@ -3,35 +3,51 @@ import 'package:flutter/material.dart';
 import 'package:taskmanager/style/style.dart';
 import 'package:taskmanager/utility/utility.dart';
 
-AppBar TaskAppBar()
-{
+AppBar TaskAppBar() {
   return AppBar(
     backgroundColor: colorGreen,
-
-   flexibleSpace:Container(
-     margin: EdgeInsets.fromLTRB(10,40,20,0),
-     child: Row(
-       children: [
-         CircleAvatar(backgroundColor:Colors.transparent,
-           radius: 24,
-           child: ClipOval(
-             child: Image.memory(ShowBase64Image("Base64String"),
-
-             ),
-           ),
-
-         ),
-         SizedBox(height: 10,),
-         Column(mainAxisAlignment:MainAxisAlignment.center,
-           crossAxisAlignment:CrossAxisAlignment.center,
-           children: [
-             Text("Ara"),
-             Text("@ara"),
-           ],),
-
-       ],
-     ),
-   ) ,
-
+    flexibleSpace: Container(
+      margin: EdgeInsets.fromLTRB(10, 40, 20, 0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 24,
+            child: ClipOval(
+              child: Image.memory(
+                ShowBase64Image("Base64String"),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Ara",
+                style: Head7Style(colorWhite),
+              ),
+              Text(
+                "@ara",
+                style: Head9Style(colorWhite),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.circle_outlined),
+      ),
+      IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.output),
+      )
+    ],
   );
 }
