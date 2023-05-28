fc557cd3aff36cd2b1ffa8885329bf10742a134f
s.m.israt <smisratara25@gmail.com>
2023-05-28 07:58:23 +0600

    Re-Usable TaskList created

---
+++ lib/component/taskList.dart
@@ -0,0 +1,21 @@
+import 'dart:js';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taskmanager/style/style.dart';
+
+ListView TaskList(TaskItems) {
+  return ListView.builder(
+      itemCount: TaskItems.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ItemSizebox(
+               Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(TaskItems[index]['title'],style: Head6Style(colorDarkBlue),),
+              Text(TaskItems[index]['title'],style: Head7Style(colorLightGray),)],
+          )),
+        );
+      });
+}
