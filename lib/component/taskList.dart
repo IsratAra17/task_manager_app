4513e1fb2dac56456245fd9ac1fecdf3c48b39e6
s.m.israt <smisratara25@gmail.com>
2023-05-29 17:01:48 +0600

    Status section updated and solved error!

--- lib/component/taskList.dart
+++ lib/component/taskList.dart
@@ -11,16 +11,16 @@ ListView TaskList(TaskItems) {
       {
 
         Color statusColor=colorGreen;
-        if(TaskItems['index']['status']=="New")
+        if(TaskItems[index]['status']=="New")
           {
             statusColor=colorBlue;
           }
 
-       else if(TaskItems['index']['status']=="Progress")
+       else if(TaskItems[index]['status']=="Progress")
         {
           statusColor=colorOrange;
         }
-       else if(TaskItems['index']['status']=="Canceled")
+       else if(TaskItems[index]['status']=="Canceled")
         {
           statusColor=colorRed;
         }
@@ -34,14 +34,17 @@ ListView TaskList(TaskItems) {
             children: [
               Text(TaskItems[index]['title'],style: Head6Style(colorDarkBlue),),
               Text(TaskItems[index]['description'],style: Head7Style(colorLightGray),),
-              Text(TaskItems[index]['createDate'],style: Head7Style(colorDarkBlue),),
+              Text(TaskItems[index]['createdDate'],style: Head9Style(colorDarkBlue),),
               SizedBox(height: 20,),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+
                   StatusChild(TaskItems[index]['status'],statusColor),
                   Container(child: Row(
                     children: [
                       SizedBox(height: 30,width: 50,child: ElevatedButton(onPressed: (){},child: Icon(Icons.edit_location_alt_outlined,size: 16,),style: AppStatusBtnStyle(colorBlue),),),
+                      SizedBox(width: 10,),
                       SizedBox(height: 30,width: 50,child: ElevatedButton(onPressed: (){},child: Icon(Icons.delete_outline,size: 16,),style: AppStatusBtnStyle(colorRed),),),
                     ],
                   ),)
