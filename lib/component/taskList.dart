cb7da066ddfd157a14ddb13253b3873b3fd705fb
s.m.israt <smisratara25@gmail.com>
2023-05-29 16:40:30 +0600

    added Date ,statusbutton UI, and edit and delete funtionality

--- lib/component/taskList.dart
+++ lib/component/taskList.dart
@@ -14,7 +14,19 @@ ListView TaskList(TaskItems) {
             children: [
               Text(TaskItems[index]['title'],style: Head6Style(colorDarkBlue),),
               Text(TaskItems[index]['description'],style: Head7Style(colorLightGray),),
-
+              Text(TaskItems[index]['createDate'],style: Head7Style(colorDarkBlue),),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  StatusChild(TaskItems[index]['status'],statusColor),
+                  Container(child: Row(
+                    children: [
+                      SizedBox(height: 30,width: 50,child: ElevatedButton(onPressed: (){},child: Icon(Icons.edit_location_alt_outlined,size: 16,),style: AppStatusBtnStyle(colorBlue),),),
+                      SizedBox(height: 30,width: 50,child: ElevatedButton(onPressed: (){},child: Icon(Icons.delete_outline,size: 16,),style: AppStatusBtnStyle(colorRed),),),
+                    ],
+                  ),)
+                ],
+              )g
 
             ],
           )),
